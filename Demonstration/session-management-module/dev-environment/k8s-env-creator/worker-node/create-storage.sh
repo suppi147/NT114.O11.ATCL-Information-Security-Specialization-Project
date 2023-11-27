396e51674f6eccd0a962d58aefa711a97dc10d6c
Tran Le Khoi <97881547+suppi147@users.noreply.github.com>
2023-11-27 21:51:43 +0700

    db creator and executioner

---
+++ Demonstration/session-management-module/dev-environment/k8s-env-creator/worker-node/create-storage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir /mnt/session-management-storage
+#copy /db-inniator/init.sql to /mnt/session-management-storage
+mkdir /mnt/session-sql-init
---
+++ Demonstration/session-management-module/dev-environment/k8s-env-executioner/worker-node/db-remove.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -r /mnt/session-management-storage/*
